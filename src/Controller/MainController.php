1be47b3238956a06b1a58545bfcdc3a8d5dbc897
Corentin Marillier <marillier.io@gmail.com>
2022-02-24 15:46:04 +0100

    add contact and portfolio page twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,11 +9,31 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="accueil")
      */
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/portfolio", name="portfolio")
+     */
+    public function portfolio(): Response
+    {
+        return $this->render('main/portfolio.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('main/contact.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
